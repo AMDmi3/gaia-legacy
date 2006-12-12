2fcd46c107f12cc7e8b0499817cc534029e00633
neo237 <neo237@6c2f56dd-5223-0410-8aad-addf9322a0be>
2006-12-12 16:21:46 +0000

    filesystem cache import included
    git-svn-id: svn+ssh://svn.code.sf.net/p/gaia-clean/code/gaia/branches/gaia-tiledb@56 6c2f56dd-5223-0410-8aad-addf9322a0be

---
+++ lib/tiledb/test.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "tiledb_filesystem_import.h"
+#include "tiledb.h"
+
+int main(int argc, char** argv)
+{
+	tiledb_filesystem_import();
+	exit(0);
+
+	DB_Handle *cache = tiledb_open(".", CREATE_IF_NOT_EXISTS);
+
+	char* hello = "HELLO WORLD ;-) ";
+	tiledb_put(cache, 0, 0, 15, hello, strlen(hello));
+	printf("\n");
+
+	if (tiledb_get(cache, 0, 0, 15) == TILEDB_OK) {
+		tiledb_get_data_ptr(cache)[tiledb_get_data_size(cache)-1] = 0;
+		printf("%s\n", tiledb_get_data_ptr(cache));
+	}
+
+	tiledb_close(cache);
+}
