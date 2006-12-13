b70e4b1e0cd0a1915ebddaf8f5493bb99dcd5311
neo237 <neo237@6c2f56dd-5223-0410-8aad-addf9322a0be>
2006-12-13 20:15:08 +0000

    changed sized types to unsized types included get_file_size function
    git-svn-id: svn+ssh://svn.code.sf.net/p/gaia-clean/code/gaia/branches/gaia-tiledb@67 6c2f56dd-5223-0410-8aad-addf9322a0be

--- lib/tiledb/tiledb_file_io.h
+++ lib/tiledb/tiledb_file_io.h
@@ -4,9 +4,12 @@
 #include <stdint.h>
 #include "tiledb_config.h"
 
-void store_data_to_file(int file, uint32_t offset, void* data, uint32_t size);
-uint32_t read_data_to_buffer(int file, uint32_t offset, void* buffer, uint32_t size);
 
-uint32_t get_cache_version(DB_Handle* db_handle);
+size_t tiledb_get_file_size(int file);
+
+void store_data_to_file(int file, uint32_t offset, void* data, size_t size);
+uint32_t read_data_to_buffer(int file, uint32_t offset, void* buffer, size_t size);
+
+uint32_t tiledb_get_version(DB_Handle* db_handle);
 
 #endif
