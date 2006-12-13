4ea209134285316760cc1eeb2aeb850c591b5255
neo237 <neo237@6c2f56dd-5223-0410-8aad-addf9322a0be>
2006-12-13 20:14:51 +0000

    changed sized types to unsized types included get_file_size function
    git-svn-id: svn+ssh://svn.code.sf.net/p/gaia-clean/code/gaia/branches/gaia-tiledb@66 6c2f56dd-5223-0410-8aad-addf9322a0be

--- lib/tiledb/tiledb_file_io.c
+++ lib/tiledb/tiledb_file_io.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <sys/stat.h>
 
 #include "tiledb_file_io.h"
 
@@ -11,7 +12,13 @@
 #define io_printf(...) while(0){}
 #endif
 
-void store_data_to_file(int file, uint32_t offset, void* data, uint32_t size)
+size_t tiledb_get_file_size(int file) {
+	struct stat file_status;
+	fstat(file, &file_status);
+	return file_status.st_size;
+}
+
+void store_data_to_file(int file, uint32_t offset, void* data, size_t size)
 {
 	io_printf("store_data_to_file(offset=%d, size=%d)\n", offset, size);
 	if (lseek(file, offset, 0) == -1) {
@@ -24,7 +31,7 @@ void store_data_to_file(int file, uint32_t offset, void* data, uint32_t size)
 	}
 }
 
-uint32_t read_data_to_buffer(int file, uint32_t offset, void* buffer, uint32_t size)
+uint32_t read_data_to_buffer(int file, uint32_t offset, void* buffer, size_t size)
 {
 	io_printf("read_data_to_buffer(offset=%d, size=%d) %s:%d\n", offset, size, __FILE__, __LINE__);
 	if (lseek(file, offset, 0) == -1) {
@@ -38,7 +45,7 @@ uint32_t read_data_to_buffer(int file, uint32_t offset, void* buffer, uint32_t s
 	return size;
 }
 
-uint32_t get_cache_version(DB_Handle* db_handle) {
+uint32_t tiledb_get_version(DB_Handle* db_handle) {
 	if (lseek(db_handle->index_file, 0, 0) == -1) {
 		db_error("on lseek");
 		exit(-1);
