f4e415ffc53a51041f5b5ef4f4fc462e84144230
amdmi3 <amdmi3@6c2f56dd-5223-0410-8aad-addf9322a0be>
2008-02-28 07:43:15 +0000

    Move to GPL v3
    git-svn-id: svn+ssh://svn.code.sf.net/p/gaia-clean/code/gaia/branches/gaia-ng@168 6c2f56dd-5223-0410-8aad-addf9322a0be

--- src/libgaia/FlatEarthView.h
+++ src/libgaia/FlatEarthView.h
@@ -1,20 +1,20 @@
-/*
- * gaia - opensource 3D interface to the planet
- * Copyright (C) 2006 gaia project
+/* 
+ * Copyright (C) 2006-2008 Dmitry Marakasov
  *
- * This program is free software; you can redistribute it and/or modify
+ * This file is part of Gaia.
+ *
+ * Gaia is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
+ * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
+ * Gaia is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ * You should have received a copy of the GNU General Public License
+ * along with Gaia.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef __GAIA__FLATEARTHVIEW_H__
